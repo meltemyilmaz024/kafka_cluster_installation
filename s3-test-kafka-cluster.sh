9f466a0680ce71bd48cdd2f031ff5e43f83f60a5
Meltem YILMAZ <26241718+meltemyilmaz024@users.noreply.github.com>
2023-12-28 08:05:27 +0300

    Update s3-test-kafka-cluster.sh

--- s3-test-kafka-cluster.sh
+++ s3-test-kafka-cluster.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+
 bin/kafka-topics.sh --bootstrap-server kafka01:9092 --create --topic first_topic
 
-# we can create topics with replication-factor 3 now!
+# Create topics with replication-factor 1
 bin/kafka-topics.sh --bootstrap-server kafka01:9092 --create --topic second_topic --replication-factor 1 --partitions 3
 
-
-
-# we can publish data to Kafka using the bootstrap server list!
-bin/kafka-console-producer.sh --broker-list kafka01:9092,kafka02:9092,kafka03:9092 --topic second_topic
+# Publish data to Kafka using the bootstrap server list!
+bin/kafka-console-producer.sh --bootstrap-server kafka01:9092,kafka02:9092,kafka03:9092 --topic second_topic
 
 # we can read data using any broker too!
 bin/kafka-console-consumer.sh --bootstrap-server kafka01:9092,kafka2:9092,kafka3:9092 --topic second_topic --from-beginning
-
 
 # let's list topics
 bin/kafka-topics.sh --bootstrap-server kafka01:9092 --list
